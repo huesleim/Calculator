72becd2c5d216e4cc812234925d71c8a74569d4d
Hueslei <234298003+huesleim@users.noreply.github.com>
2025-12-20 10:28:36 -0300

    Formatting fix.

--- script.js
+++ script.js
@@ -15,35 +15,24 @@ const root = document.documentElement;
 
 // event listeners
 
-numbers.forEach(button => {
+numbers.forEach((button) => {
     button.addEventListener("click", () => {
-      if (currentNumber.length === 12) {
-        return;
-      }
+        if (currentNumber.length === 12) {
+            return;
+        }
         if (result !== undefined) {
             currentNumber = "";
             result = undefined;
         }
-        if (
-            button.textContent.trim() === "." &&
-            currentNumber.includes(".")
-        ) return;
-        if (
-            currentNumber === "0" &&
-            button.textContent.trim() === "0"
-        ) return;
-        if (
-            button.textContent.trim() === "." &&
-            currentNumber === ""
-        ) {
+        if (button.textContent.trim() === "." && currentNumber.includes("."))
+            return;
+        if (currentNumber === "0" && button.textContent.trim() === "0") return;
+        if (button.textContent.trim() === "." && currentNumber === "") {
             currentNumber = "0.";
             refreshDisplay();
             return;
         }
-        if (
-            currentNumber === "0" &&
-            button.textContent.trim() !== "."
-        ) {
+        if (currentNumber === "0" && button.textContent.trim() !== ".") {
             currentNumber = button.textContent.trim();
             refreshDisplay();
             return;
@@ -53,13 +42,14 @@ numbers.forEach(button => {
     });
 });
 
-operators.forEach(button => {
+operators.forEach((button) => {
     button.addEventListener("click", () => {
         operator = button.textContent.trim();
         refreshDisplay(operator);
         firstNumber = currentNumber;
         currentNumber = "";
-    })});
+    });
+});
 
 equalButton.addEventListener("click", () => {
     if (result === currentNumber) {
@@ -115,43 +105,39 @@ function skinChange(event) {
 
     switch (value) {
         case "dark":
-            root.style.setProperty('--numbers-background-color', 'lightgrey');
-         root.style.setProperty('--operators-background-color', 'darkgrey');
-         root.style.setProperty('--display-background-color', 'white');
-         root.style.setProperty('--clear-background-color', '#3B514A');
-         root.style.setProperty('--equalTo-background-color', 'darkgrey');
-         root.style.setProperty('--text-background-color', 'black');
-         root.style.setProperty('--display-text-color', '#2D423C');
-                root.style.setProperty('--background-color','#2D423C' );
-        root.style.setProperty('--button-border-color', '#2D423C');
+            root.style.setProperty("--numbers-background-color", "lightgrey");
+            root.style.setProperty("--operators-background-color", "darkgrey");
+            root.style.setProperty("--display-background-color", "white");
+            root.style.setProperty("--clear-background-color", "#3B514A");
+            root.style.setProperty("--equalTo-background-color", "darkgrey");
+            root.style.setProperty("--text-background-color", "black");
+            root.style.setProperty("--display-text-color", "#2D423C");
+            root.style.setProperty("--background-color", "#2D423C");
+            root.style.setProperty("--button-border-color", "#2D423C");
             break;
-        
+
         case "light":
-        root.style.setProperty('--numbers-background-color', '#ededed');
-         root.style.setProperty('--operators-background-color', 'lightblue');
-         root.style.setProperty('--display-background-color', ' #ededed');
-         root.style.setProperty('--clear-background-color', 'orange');
-         root.style.setProperty('--equalTo-background-color', '#E50010');
-         root.style.setProperty('--text-background-color', 'black');
-         root.style.setProperty('--display-text-color', 'darkgrey');
-                root.style.setProperty('--background-color','#ededed' );    
-        root.style.setProperty('--button-border-color', 'grey');
-        break;
-        
-        
+            root.style.setProperty("--numbers-background-color", "#ededed");
+            root.style.setProperty("--operators-background-color", "lightblue");
+            root.style.setProperty("--display-background-color", " #ededed");
+            root.style.setProperty("--clear-background-color", "orange");
+            root.style.setProperty("--equalTo-background-color", "#E50010");
+            root.style.setProperty("--text-background-color", "black");
+            root.style.setProperty("--display-text-color", "darkgrey");
+            root.style.setProperty("--background-color", "#ededed");
+            root.style.setProperty("--button-border-color", "grey");
+            break;
+
         case "pastel":
-        root.style.setProperty('--numbers-background-color', '#E6E6B9');
-         root.style.setProperty('--operators-background-color', '#B8E6E0');
-         root.style.setProperty('--display-background-color', 'white');
-         root.style.setProperty('--clear-background-color', '#664333');
-         root.style.setProperty('--equalTo-background-color', '#E6C6B8');
-         root.style.setProperty('--text-background-color', 'black');
-         root.style.setProperty('--display-text-color', '#664333');
-        root.style.setProperty('--background-color','#E6E6B9' );
-                root.style.setProperty('--button-border-color', '#664333');
-        break;
+            root.style.setProperty("--numbers-background-color", "#E6E6B9");
+            root.style.setProperty("--operators-background-color", "#B8E6E0");
+            root.style.setProperty("--display-background-color", "white");
+            root.style.setProperty("--clear-background-color", "#664333");
+            root.style.setProperty("--equalTo-background-color", "#E6C6B8");
+            root.style.setProperty("--text-background-color", "black");
+            root.style.setProperty("--display-text-color", "#664333");
+            root.style.setProperty("--background-color", "#E6E6B9");
+            root.style.setProperty("--button-border-color", "#664333");
+            break;
     }
 }
-
-
-
